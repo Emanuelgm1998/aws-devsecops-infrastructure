ce7bd05277bbb90b2a7584d9fea557e1b1f0601b
Emanuel Gonzalez <TU_CORREO_DE_GITHUB>
2026-07-21 07:30:38 -0400

    security: reemplaza secretos hardcodeados por random_password

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,3 +1,3 @@
-variable "aws_region"      { default = "us-east-1" }
-variable "project"         { default = "secure-saas" }
+variable "aws_region" { default = "us-east-1" }
+variable "project" { default = "secure-saas" }
 variable "container_image" { default = "cloudz777/secure-saas-platform:latest" }
--- terraform/modules/ecs/secrets.tf
+++ terraform/modules/ecs/secrets.tf
@@ -1,13 +1,23 @@
+resource "random_password" "db_password" {
+  length  = 24
+  special = true
+}
+
+resource "random_password" "api_key" {
+  length  = 24
+  special = true
+}
+
 resource "aws_secretsmanager_secret" "app" {
   name                    = "${var.project}/app-secrets"
   recovery_window_in_days = 0
-  tags = { Name = "${var.project}-secrets" }
+  tags                    = { Name = "${var.project}-secrets" }
 }
 
 resource "aws_secretsmanager_secret_version" "app" {
   secret_id = aws_secretsmanager_secret.app.id
   secret_string = jsonencode({
-    DB_PASSWORD = "secure-placeholder-password"
-    API_KEY     = "secure-placeholder-api-key"
+    DB_PASSWORD = random_password.db_password.result
+    API_KEY     = random_password.api_key.result
   })
 }
